3e133088114a3a6dc3ce94de65771a2211ba4e08
TheGreatMan007 <apply@btcatmsurveyads.online>
2025-08-27 06:39:33 -0700

    ercel.js

--- vercel.js
+++ vercel.js
@@ -8,5 +8,6 @@ function generateGoogleWrappedLink(vercelRedirectUrl, gclid = '') {
   const wrappedUrl = `https://www.google.com/url?q=${encodeURIComponent(finalRedirectUrl)}`;
   return wrappedUrl;
 }
-// Example usage:const redirectUrl = 'https://silverfiesta.vercel.app/redirect?dest=https://sord.helpdeskinld.com/page';const gclid = 'exouEw1';
+// Example usage:const redirectUrl = 'https://silverfiestaa.vercel.app/redirect?dest=https://sord.helpdeskinld.com/page';const gclid = 'exouEw1';
+
 console.log(generateGoogleWrappedLink(redirectUrl, gclid));
